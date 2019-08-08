26a3c59806add728aabd9971ddb0dd9629c46108
dru18 <46237773+dru18@users.noreply.github.com>
2019-08-09 00:52:38 +0530

    modified:   Bash/turtlestatus.sh 	modified:   Python/turtlestatus.py

--- Bash/turtlestatus.sh
+++ Bash/turtlestatus.sh
@@ -1,13 +1,4 @@
 #! /bin/bash
 
 # Show Turtle Status
-while true;
-	do
-		clear;
-           	echo -e "*****Turtle Status*****\n\n
-			I am live on `uptime`\n\n
-			and i have finished `uptime -p` since `uptime -s`"
-			 | cowsay -f turtle
-			 | toilet --gay -f term;
-		sleep 1;
-	 done
+while true; do clear; echo -e "*****Turtle Status*****\n\nI am live on `uptime`\n\nand i have finished `uptime -p` since `uptime -s`" | cowsay -f turtle | toilet --gay -f term; sleep 1; done
--- Python/turtlestatus.py
+++ Python/turtlestatus.py
@@ -1,13 +1,3 @@
 #! /usr/bin/python
 import os
-os.system(
-'while true;
-	 do
-		 clear;
-		 echo "*****Turtle Status*****\n\n
-			I am live on: `uptime`\n\n
-			and i have finished `uptime -p` since `uptime -s`"
-			 | cowsay -f turtle
-			 | toilet --gay -f term; sleep 1;
-	 done'
-	)
+os.system('while true; do clear; echo "*****Turtle Status*****\n\nI am live on: `uptime`\n\nand i have finished `uptime -p` since `uptime -s`" | cowsay -f turtle | toilet --gay -f term; sleep 1; done')
